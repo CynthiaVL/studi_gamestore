9e53ea3d91d0b10d9a501eddb5c315c279eb6343
CynthiaVl <cynthia.vanlaer@gmail.com>
2024-06-30 15:00:01 +0200

    GameController Crud

---
+++ src/Form/GameType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Game;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GameType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('pegi')
+            ->add('genre')
+            ->add('price')
+            ->add('platform')
+            ->add('image')
+            ->add('release_date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('created_at', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('update_at', null, [
+                'widget' => 'single_text',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Game::class,
+        ]);
+    }
+}
